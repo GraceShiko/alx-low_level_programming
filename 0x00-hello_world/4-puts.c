1288ac34c034c3bc9a7dc15662f502a5a865e069
root <graciewshiko@gmail.com>
2022-03-14 17:34:00 -0700

    pushing changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>                                                                    
-/**                                                                                   
- * main - Entry point                                                                 
- * Description: prints ""Programming is like building a multilingual puzzle\n"        
- * Return: 0                                                                          
- */                                                                                   
-int main(void)                                                                        
-{                                                                                     
-  puts("\"Programming is like building a multilingual puzzle");                         
-  return (0);                                                                           
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
