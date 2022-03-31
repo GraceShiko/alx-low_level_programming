980e5c8e6fcb83bd10dcece3bc3c0c3b067b620f
root <graciewshiko@gmail.com>
2022-03-31 14:43:31 -0700

    pushing changes

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - is a function that prints a string.
+ *
+ * @s: is a pointer to a char array
+ *
+ * Return: Return the transformed poin
+ **/
+void _print_rev_recursion(char *s)
+{
+if (*s == 0)
+{
+return;
+}
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
