c0c885430e0ead4b2b6a8bd33bfa0bbf22da2687
root <graciewshiko@gmail.com>
2022-04-06 18:39:58 -0700

    pushing changes

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -54,37 +54,28 @@ char **strings;
 int index = 0, words, w, letters, l;
 if (str == NULL || str[0] == '\0')
 return (NULLwords = count_words(str);
-  if (words == 0)
-    return (NULL);
-
-  strings = malloc(sizeof(char *) * (words + 1));
-  if (strings == NULL)
-    return (NULL);
-
-  for (w = 0; w < words; w++)
-    {
-      while (str[index] == ' ')
-	index++;
-
-      letters = word_len(str + index);
-
-      strings[w] = malloc(sizeof(char) * (letters + 1));
-
-      if (strings[w] == NULL)
-	{
-	  for (; w >= 0; w--)
-	    free(strings[w]);
-
-	  free(strings);
-	  return (NULL);
-	}
-
-      for (l = 0; l < letters; l++)
-	strings[w][l] = str[index++];
-
-      strings[w][l] = '\0';
-    }
-  strings[w] = NULL;
-
-  return (strings);
+if (words == 0)
+return (NULL);
+strings = malloc(sizeof(char *) * (words + 1));
+if (strings == NULL)
+return (NULL);
+for (w = 0; w < words; w++)
+{
+while (str[index] == ' ')
+index++;
+letters = word_len(str + index);
+strings[w] = malloc(sizeof(char) * (letters + 1));
+if(strings[w] == NULL)
+{
+for (; w >= 0; w--)
+free(strings[w]);
+free(strings);
+return (NULL);
+}
+for (l = 0; l < letters; l++)
+strings[w][l] = str[index++];
+strings[w][l] = '\0';
+}
+strings[w] = NULL;
+return (strings);
 }
