0716da2f44b7a8afe35a08b7c43fff84b48b95da
root <graciewshiko@gmail.com>
2022-04-06 18:31:28 -0700

    pushing changes

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,36 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- * argstostr - concatenate all arguments of your program with newline
- * @ac: argument count
- * @av: double pointer to array of strings passed to main
- * Return: Null if fail, else return pointer to new string
+ * argstostr - concatenates arguments.
+ * @ac: number of string to be concatenated.
+ * @av: strings to be concatenated.
+ * Return: String concatenated.
  */
 char *argstostr(int ac, char **av)
 {
-char *a, *retp;
-int i, j, total;
+int i, j, k = 0;
+char *concat = NULL;
 if (ac == 0 || av == NULL)
 return (NULL);
-for (i = 0, total = 0; i < ac; i++)
-{
-for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
-;
-total++;
-}
-total++;
-a = malloc(total * sizeof(char));
-if (a == NULL)
-return (NULL);
-retp = a;
 for (i = 0; i < ac; i++)
-{
 for (j = 0; av[i][j] != '\0'; j++)
+k++;
+concat = malloc(sizeof(char) * (k + ac + 1));
+if (concat == NULL)
+return (NULL);
+for (i = 0, k = 0; i < ac; i++)
 {
-a++;
+for (j = 0; av[i][j] != '\0'; j++, k++)
+concat[k] = av[i][j];
+concat[k] = '\n';
+k++;
 }
-a++;
-}
-return (retp);
+concat[k] = '\0';
+return (concat);
 }
